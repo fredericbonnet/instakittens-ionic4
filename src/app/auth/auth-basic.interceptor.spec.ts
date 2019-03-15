ec292293a273a5302d5671f06d3cbcd598a4ccc3
Frederic Bonnet <fredericbonnet@free.fr>
2019-03-15 16:41:00 +0100

    Added unit tests for core services

--- src/app/auth/auth-basic.interceptor.spec.ts
+++ src/app/auth/auth-basic.interceptor.spec.ts
@@ -1,20 +1,85 @@
 import { TestBed } from '@angular/core/testing';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { AuthBasicInterceptor } from './auth-basic.interceptor';
 import { AuthService } from './auth.service';
 
 describe('AuthBasicInterceptor', () => {
+  let interceptor: AuthBasicInterceptor;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let authServiceSpy;
 
   beforeEach(() => {
     authServiceSpy = jasmine.createSpyObj('AuthService', ['loadAuthData']);
+
     TestBed.configureTestingModule({
-      providers: [{ provide: AuthService, useValue: authServiceSpy }],
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthBasicInterceptor,
+          multi: true,
+        },
+      ],
     });
+
+    interceptor = TestBed.get(AuthBasicInterceptor);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    const interceptor: AuthBasicInterceptor = TestBed.get(AuthBasicInterceptor);
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should not add an Authorization header when no authentication data is present', () => {
+    authServiceSpy.loadAuthData.and.stub();
+
+    // Build request.
+    const testData = 'test data';
+    httpClient.get('/data').subscribe(data => {
+      expect(data).toEqual(testData);
+    });
+
+    // HTTP expectations.
+    const req = httpTestingController.expectOne('/data');
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.headers.has('Authorization')).toBeFalsy();
+
+    // Trigger response.
+    req.flush(testData);
+  });
+  it('should add an Authorization header when authentication data is present', () => {
+    // Mock auth data.
+    const authData = { username: 'testuser', password: 'password' };
+    authServiceSpy.loadAuthData.and.returnValue(authData);
+
+    // Build request.
+    const testData = 'test data';
+    httpClient.get('/data').subscribe(data => {
+      expect(data).toEqual(testData);
+    });
+
+    // HTTP expectations.
+    const req = httpTestingController.expectOne('/data');
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.headers.has('Authorization')).toBeTruthy();
+    const authorization =
+      'Basic ' + btoa(authData.username + ':' + authData.password);
+    expect(req.request.headers.get('Authorization')).toEqual(authorization);
+
+    // Trigger response.
+    req.flush(testData);
   });
 });
--- src/app/auth/auth-error.interceptor.spec.ts
+++ src/app/auth/auth-error.interceptor.spec.ts
@@ -1,12 +1,98 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { AuthErrorInterceptor } from './auth-error.interceptor';
+import { LoginRequiredError, AccessDeniedError } from './errors';
 
 describe('AuthErrorInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let interceptor: AuthErrorInterceptor;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthErrorInterceptor,
+          multi: true,
+        },
+      ],
+    });
+
+    interceptor = TestBed.get(AuthErrorInterceptor);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const interceptor: AuthErrorInterceptor = TestBed.get(AuthErrorInterceptor);
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should rethrow 401 errors as LoginRequiredError', () => {
+    const message = 'error message';
+
+    // Build request.
+    httpClient.get('/data').subscribe(
+      data => fail('should have failed with LoginRequiredError'),
+      err => {
+        expect(err instanceof LoginRequiredError).toBeTruthy();
+        expect(err.message).toEqual(message);
+      }
+    );
+
+    // HTTP expectations.
+    const req = httpTestingController.expectOne('/data');
+    expect(req.request.method).toEqual('GET');
+
+    // Trigger error.
+    req.flush(message, { status: 401, statusText: message });
+  });
+  it('should rethrow 403 errors as AccessDeniedError', () => {
+    const message = 'error message';
+
+    // Build request.
+    httpClient.get('/data').subscribe(
+      data => fail('should have failed with AccessDeniedError'),
+      err => {
+        expect(err instanceof AccessDeniedError).toBeTruthy();
+        expect(err.message).toEqual(message);
+      }
+    );
+
+    // HTTP expectations.
+    const req = httpTestingController.expectOne('/data');
+    expect(req.request.method).toEqual('GET');
+
+    // Trigger error.
+    req.flush(message, { status: 403, statusText: message });
+  });
+  it('should let other errors pass through', () => {
+    const message = 'error message';
+
+    // Build request.
+    httpClient.get('/data').subscribe(
+      data => fail('should have failed with HttpErrorResponse'),
+      err => {
+        expect(err instanceof HttpErrorResponse).toBeTruthy();
+        expect(err.error.message).toEqual(message);
+      }
+    );
+
+    // HTTP expectations.
+    const req = httpTestingController.expectOne('/data');
+    expect(req.request.method).toEqual('GET');
+
+    // Trigger error.
+    req.error(new ErrorEvent('Unknown error', { message }));
   });
 });
