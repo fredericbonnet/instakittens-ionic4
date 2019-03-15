08b09cb8f765166cd05a03db8c2e974018b9b585
Frederic Bonnet <fredericbonnet@free.fr>
2019-03-15 16:41:00 +0100

    Added unit tests for core services

--- src/app/app-error-handler.service.spec.ts
+++ src/app/app-error-handler.service.spec.ts
@@ -1,20 +1,59 @@
 import { TestBed } from '@angular/core/testing';
+import { ToastController } from '@ionic/angular';
 
 import { AppErrorHandlerService } from './app-error-handler.service';
 import { AuthService } from './auth/auth.service';
+import { LoginRequiredError } from './auth/errors';
 
 describe('AppErrorHandlerService', () => {
-  let authServiceSpy;
+  let service: AppErrorHandlerService;
+  let authServiceSpy, toastControllerSpy;
 
   beforeEach(() => {
-    authServiceSpy = jasmine.createSpyObj('AuthService', ['login']);
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['openLoginPage']);
+    toastControllerSpy = jasmine.createSpyObj('ToastController', ['create']);
+
     TestBed.configureTestingModule({
-      providers: [{ provide: AuthService, useValue: authServiceSpy }],
+      providers: [
+        { provide: AuthService, useValue: authServiceSpy },
+        { provide: ToastController, useValue: toastControllerSpy },
+      ],
     });
+
+    service = TestBed.get(AppErrorHandlerService);
   });
 
   it('should be created', () => {
-    const service: AppErrorHandlerService = TestBed.get(AppErrorHandlerService);
     expect(service).toBeTruthy();
+  });
+
+  it('should display errors messages in a toast', async () => {
+    const message = 'error message';
+    const toast = jasmine.createSpyObj('toast', ['present']);
+    toastControllerSpy.create.and.returnValue(Promise.resolve(toast));
+
+    await service.handleError(message);
+    await service.handleError(new Error(message));
+    await service.handleError({ rejection: new Error(message) });
+
+    // Expectations.
+    expect(authServiceSpy.openLoginPage).not.toHaveBeenCalled();
+    expect(toastControllerSpy.create).toHaveBeenCalledTimes(3);
+    expect(toast.present).toHaveBeenCalledTimes(3);
+    for (let i = 0; i < 3; i++) {
+      expect(toastControllerSpy.create.calls.argsFor(i)[0].message).toEqual(
+        message
+      );
+    }
+  });
+
+  it('should redirect to login page upon LoginRequiredError', async () => {
+    const error = new LoginRequiredError('error message');
+
+    await service.handleError({ rejection: error });
+
+    // Expectations.
+    expect(authServiceSpy.openLoginPage).toHaveBeenCalled();
+    expect(toastControllerSpy.create).not.toHaveBeenCalled();
   });
 });
--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,20 +1,126 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { Router } from '@angular/router';
 import { EMPTY } from 'rxjs';
 
 import { AuthService } from './auth.service';
+import { environment } from '../../environments/environment';
 
 describe('AuthService', () => {
+  let service: AuthService;
+  let httpTestingController: HttpTestingController;
+  let routerSpy;
+
   beforeEach(() => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    routerSpy.events = EMPTY;
+
+    // Local storage mock.
+    let localStorageStore = {};
+    spyOn(localStorage, 'getItem').and.callFake(key => {
+      return localStorageStore[key] || null;
+    });
+    spyOn(localStorage, 'removeItem').and.callFake(key => {
+      delete localStorageStore[key];
+    });
+    spyOn(localStorage, 'setItem').and.callFake((key, value) => {
+      return (localStorageStore[key] = value);
+    });
+    spyOn(localStorage, 'clear').and.callFake(() => {
+      localStorageStore = {};
+    });
+
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [{ provide: Router, useValue: { events: EMPTY } }],
+      providers: [{ provide: Router, useValue: routerSpy }],
     });
+
+    service = TestBed.get(AuthService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
+  });
+
+  it('should store authentication data in local storage', () => {
+    const authData = 'auth data';
+    expect(service.loadAuthData()).toBeNull();
+    expect(localStorage.getItem).toHaveBeenCalled();
+    service.storeAuthData(authData);
+    expect(localStorage.setItem).toHaveBeenCalled();
+    expect(service.loadAuthData()).toEqual(authData);
+    service.clearAuthData();
+    expect(localStorage.removeItem).toHaveBeenCalled();
+    expect(service.loadAuthData()).toBeNull();
+  });
+
+  it('should call authentication service when given credentials', () => {
+    const username = 'testuser';
+    const password = 'testpassord';
+    const authData = 'authdata';
+
+    service.auth(username, password).subscribe(data => {
+      expect(data).toEqual(authData);
+    });
+
+    // HTTP expectations.
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/auth`);
+    expect(req.request.method).toEqual('GET');
+
+    // Trigger response.
+    req.flush(authData);
+  });
+
+  it('should store credentials upon successful authentication', done => {
+    const username = 'testuser';
+    const password = 'testpassord';
+    const authData = 'authdata';
+
+    expect(service.loadAuthData()).toBeNull();
+    service.auth(username, password).subscribe(data => {
+      expect(service.loadAuthData()).not.toBeNull();
+      done();
+    });
+    expect(service.loadAuthData()).not.toBeNull();
+
+    // HTTP expectations.
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/auth`);
+    expect(req.request.method).toEqual('GET');
+
+    // Trigger response.
+    req.flush(authData);
+  });
+
+  it('should clear credentials upon authentication failure', done => {
+    const username = 'testuser';
+    const password = 'testpassord';
+
+    expect(service.loadAuthData()).toBeNull();
+    service.auth(username, password).subscribe(
+      data => {
+        fail('authentication request should have failed');
+      },
+      err => {
+        expect(service.loadAuthData()).toBeNull();
+        done();
+      }
+    );
+    expect(service.loadAuthData()).not.toBeNull();
+
+    // HTTP expectations.
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/auth`);
+    expect(req.request.method).toEqual('GET');
+
+    // Trigger error.
+    req.error(new ErrorEvent('Unknown error'));
   });
 });
