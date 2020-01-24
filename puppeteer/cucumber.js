d6d75f379c283047b3c829242a9baeb2ed74facd
Frederic Bonnet <fredericbonnet@free.fr>
2020-01-24 11:36:16 +0100

    Use rerun option with Cucumber in watch mode

--- puppeteer/cucumber.js
+++ puppeteer/cucumber.js
@@ -2,12 +2,23 @@
  * Cucumber-js profiles.
  */
 
+const common = `
+  --require support/globals.js 
+  --require support/assertions.js 
+  --require support/puppeteer-cucumber.js 
+  --require step_definitions/**/*.steps.js
+  `;
+
+// Rerun option.
+const fs = require('fs');
+const rerunFile = '@rerun.txt';
+let rerunOption = '';
+if (fs.existsSync(rerunFile) && fs.statSync(rerunFile).size > 0) {
+  // Activate rerun.
+  rerunOption = rerunFile;
+}
+
 module.exports = {
-  default: `
-    features/**/*.feature
-    --require support/globals.js 
-    --require support/assertions.js 
-    --require support/puppeteer-cucumber.js 
-    --require step_definitions/**/*.steps.js
-    `,
+  default: `${common} features/**/*.feature`,
+  watch: `${common} --format rerun:${rerunFile} ${rerunOption}`,
 };
